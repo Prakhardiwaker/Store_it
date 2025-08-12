4c7916d32b7d4c7f845da11aac70b50b9c8cb8b4
prakhar diwaker <prakhardiwaker@gmail.com>
2025-08-13 03:21:44 +0530

    first commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import AuthForm from "@/components/AuthForm";
+
+function SingIn() {
+  return <AuthForm type="sign-in" />;
+}
+
+export default SingIn;
---
+++ components/Search.tsx
@@ -0,0 +1,118 @@
+"use client";
+import Image from "next/image";
+import React, { useEffect, useState } from "react";
+import { Input } from "./ui/input";
+import { usePathname, useSearchParams } from "next/navigation";
+import { Models } from "node-appwrite";
+import { getFiles } from "@/lib/actions/file.actions";
+import Thumbnail from "./Thumbnail";
+import FormattedDateTime from "./FormattedDateTime";
+import { useRouter } from "next/navigation";
+import { useDebounce } from "use-debounce";
+
+// Define custom file interface extending Appwrite Document
+interface FileDocument extends Models.Document {
+  type: string;
+  extension: string;
+  url: string;
+  name: string;
+  $createdAt: string;
+}
+
+const Search = () => {
+  const [query, setQuery] = useState("");
+  const searchParams = useSearchParams();
+  const searchQuery = searchParams.get("query") || "";
+  const [results, setResults] = useState<FileDocument[]>([]);
+  const [open, setOpen] = useState(false);
+  const router = useRouter();
+  const path = usePathname();
+  const [debouncedQuery] = useDebounce(query, 300);
+
+  useEffect(() => {
+    const fetchFiles = async () => {
+      if (debouncedQuery.length === 0) {
+        setResults([]);
+        setOpen(false);
+        return router.push(path.replace(searchParams.toString(), ""));
+      }
+
+      const files = await getFiles({ types: [], searchText: debouncedQuery });
+      setResults(files.documents as FileDocument[]);
+      setOpen(true);
+    };
+
+    fetchFiles();
+  }, [debouncedQuery]);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setQuery("");
+    }
+  }, [searchQuery]);
+
+  const handleClickItem = (file: FileDocument) => {
+    setOpen(false);
+    setResults([]);
+
+    router.push(
+      `/${file.type === "video" || file.type === "audio" ? "media" : file.type + "s"}?query=${query}`
+    );
+  };
+
+  return (
+    <div className="search">
+      <div className="search-input-wrapper">
+        <Image
+          src="/assets/icons/search.svg"
+          alt="search"
+          width={24}
+          height={24}
+        />
+        <Input
+          value={query}
+          placeholder="Search..."
+          className="search-input"
+          onChange={(e) => {
+            setQuery(e.target.value);
+          }}
+        />
+
+        {open && (
+          <ul className="search-result">
+            {results.length > 0 ? (
+              results.map((file) => (
+                <li
+                  key={file.$id}
+                  className="flex items-center justify-between"
+                  onClick={() => handleClickItem(file)}
+                >
+                  <div className="flex cursor-pointer items-center gap-4">
+                    <Thumbnail
+                      type={file.type}
+                      extension={file.extension}
+                      url={file.url}
+                      className="size-9 min-w-9"
+                    />
+
+                    <p className="subtitle-2 line-clamp-1 text-light-100">
+                      {file.name}
+                    </p>
+                  </div>
+                  <FormattedDateTime
+                    date={file.$createdAt}
+                    className="caption line-clamp-1"
+                  />
+                </li>
+              ))
+            ) : (
+              <p className="empty-result">No files found</p>
+            )}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Search;
